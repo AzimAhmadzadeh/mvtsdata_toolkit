82d92b55e07fedc2d11f0a10126e62e0ca60b82d
AzimAhmadzadeh <azim.ahmadzadeh@gmail.com>
2019-11-02 19:56:20 -0400

    Added a jupyter notebook.

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name="TimeSeriesAnalyzer",
+    name="MVTS Data Toolkit",
     version="0.0.1",
-    author="Kankana Sinha & Azim Ahmadzadeh",
-    author_email="ksinha1106@gmail.com",
-    description="A package to analyse time series data",
+    author="Kankana Sinha - Azim Ahmadzadeh",
+    author_email="ksinha3@student.gsu.edu - aahmadzadeh1@cs.gsu.edu",
+    description="A python software to facilitate working with multivariate time series datasets.",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://bitbucket.org/azimdmlab/mvts_features/src/master/",
+    url="https://AzimAhmadzadeh@bitbucket.org/gsudmlab/mvts_data_toolkit.git",
     packages=find_packages(),
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
     ],
 )
