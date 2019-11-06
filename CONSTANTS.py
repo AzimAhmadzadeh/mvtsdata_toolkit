04651e2df50ec2b96d708592701d11d3db81b743
AzimAhmadzadeh <azim.ahmadzadeh@gmail.com>
2019-11-06 14:10:11 -0500

    Modified the DataRetriever class.

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -2,6 +2,6 @@ import os
 
 ROOT = os.path.dirname(__file__)
 PATH_TO_CONFIG = os.path.join(ROOT, 'feature_extraction_configs.yml')
-
-URL_DATASET_01 = 'https://bitbucket.org/gsudmlab/mvts_data_toolkit/downloads/petdataset_01.zip'
-URL_DATASET_02 = ''
+PATH_TO_DATASETS_CONFIG = os.path.join(ROOT, 'datasets_configs.yml')
+DATASETS_DICT = {1: 'https://bitbucket.org/gsudmlab/mvts_data_toolkit/downloads/petdataset_01.zip',
+                 2: ''}
--- data_retriever.py
+++ data_retriever.py
@@ -1,61 +1,107 @@
 import CONSTANTS as CONST
 from io import BytesIO
+import os
 from urllib.request import urlopen
+from urllib.parse import urlparse
 from zipfile import ZipFile
-import tqdm
 import zipfile
 import http.client
+import tqdm
 from hurry.filesize import size
+import yaml
 
 
 class DataRetriever:
     """
-    This is a simple class to provide data for the user to play around with the package's
-    functionalities through its demo notebook.
+    This is a simple class to provide data for users to play around with the package's
+    functionalities through its demo jupyter notebook.
     notebook.
     """
+    def __init__(self, dataset_number=1):
 
-    def __init__(self):
-        self.datasets = {1: CONST.URL_DATASET_01,
-                         2: CONST.URL_DATASET_02}
-        self.meta: http.client.HTTPMessage = None
+        configs: dict = self.__read_config()
+        self.dataset_url = configs['DATASETS'][dataset_number]
         self.all_members = None
+        self.dataset_name = self.__get_dataset_name()
+        self.__meta: http.client.HTTPMessage = self.__get_info()
 
-    def get_info(self, dataset_number=1):
+    @staticmethod
+    def __read_config():
         """
-        :param dataset_number:
+        reads the config file located at CONST.PATH_TO_DATASETS_CONFIG, that contains a list of
+        urls to a few datasets.
+        :return: the content of the configuration file as a dictionary.
+        """
+        path_to_config = os.path.join(CONST.ROOT, CONST.PATH_TO_DATASETS_CONFIG)
+        with open(path_to_config) as file:
+            configs = yaml.load(file, Loader=yaml.FullLoader)
+        return configs
+
+    def __get_dataset_name(self):
+        """
+        retrieves the dataset's name from the dataset's url.
         :return:
         """
-        with urlopen(self.datasets[dataset_number]) as zipresp:
-            self.meta = zipresp.info()
+        url_path = urlparse(self.dataset_url).path
+        return os.path.basename(url_path)
 
-            with ZipFile(BytesIO(zipresp.read())) as zfile:
-                self.all_members = zfile.namelist()
+    def __get_info(self):
+        """
+        retrieves some basic info about the dataset before downloading it.
+        :return:
+        """
+        with urlopen(self.dataset_url) as req:
+            meta = req.info()
+        return meta
 
     def get_total_size(self):
         """:return the size of the data to be extracted."""
-        return size(int(self.meta['Content-Length']))
+        return size(int(self.__meta['Content-Length']))
 
     def get_compression_type(self):
         """:return the compression type of the dataset."""
-        return self.meta['Content-Type']
+        return self.__meta['Content-Type']
 
     def get_total_number_of_files(self):
         """:return the total number of files in the dataset."""
-        return len(self.all_members) - 1  # -1 for the parrent directory
+        if self.all_members is None:
+            print('[!] this can be called only after the files are downloaded.')
+            return 0
+        return len(self.all_members) - 1  # -1 for the parent directory
 
-    def retrieve(self, target_path, dataset_id=1):
+    def retrieve(self, target_path):
         """
-        downloads and extracted the compressed dataset (given by 'dataset_id') into the directory
-        'target_path'.
-        :param target_path:
-        :param dataset_id:
+        downloads and extracts the compressed dataset (given by 'dataset_id') into the directory
+        'target_path'. The original compressed file won't be kept. Only it's content will be copied
+        into 'target_path'.
+        :param target_path: where the dataset should be copied to.
         :return:
         """
-        with urlopen(self.datasets[dataset_id]) as zipresp:
+        with urlopen(self.dataset_url) as zipresp:
             with ZipFile(BytesIO(zipresp.read())) as zfile:
+                self.all_members = zfile.namelist()
                 for member in tqdm.tqdm(zfile.infolist(), desc='Extracting'):
                     try:
                         zfile.extract(member, target_path)
                     except zipfile.error as e:
-                        pass
+                        print(e)
+
+    def test(self):
+        with urlopen(self.dataset_url) as zipresp:
+            self.__meta = zipresp.info()
+
+
+def main():
+    dr = DataRetriever()
+    print('URL:\t\t{}'.format(dr.dataset_url))
+    print('NAME:\t\t{}'.format(dr.dataset_name))
+    print('TYPE:\t\t{}'.format(dr.get_compression_type()))
+    print('SIZE:\t\t{}'.format(dr.get_total_size()))
+
+    print('Now let\'s download the file ...')
+    dr.retrieve(target_path='./xxx')
+    print('\n\n\t\tDONE')
+
+
+if __name__ == "__main__":
+    main()
