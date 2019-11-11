002659a6dc10ab16376d8bba97bb956b702e65bb
AzimAhmadzadeh <azim.ahmadzadeh@gmail.com>
2019-11-11 13:31:25 -0500

    Trying to resolve the sphinx issues.

--- sphinxdocs/conf.py
+++ sphinxdocs/conf.py
@@ -20,6 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('../..'))
 
 
 # -- General configuration ------------------------------------------------
@@ -32,10 +33,10 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
