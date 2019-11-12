26033fe7aed0bdc787f201088dcae243c8cf00a6
AzimAhmadzadeh <azim.ahmadzadeh@gmail.com>
2019-11-12 13:36:16 -0500

    Cleaning the docstrings.

--- features/extractor_utils.py
+++ features/extractor_utils.py
@@ -31,20 +31,22 @@ def __get_methods_for_names(method_names: list):
 def calculate_one_mvts(df_mvts: pd.DataFrame, features_list: list) -> pd.DataFrame:
     """
     This method computes a list of F statistical features on the given multivariate time series
-    of P parameters. The output is a dataframe of dimension P X F, that looks like:
-    -----------------------------------------------
-        f1    f2    ...
-    p1  val   val   ...
-    p2  val   val   ...
-    ... ...   ...   ...
-    -----------------------------------------------
+    of P parameters. The output is a dataframe of dimension P X F, that looks like::
+
+        -----------------------
+            f1    f2    ...
+        p1  val   val   ...
+        p2  val   val   ...
+        ... ...   ...   ...
+        -----------------------
+
     Note: The statistical features will be extracted from all the give columns. So, in case it is
     needed only over some of the time series, then only those selected columns should be passed in.
 
-    :param df_mvts: a mvts dataframe from which the features are to be extracted
-    :param features_list : a list of all functions (from `features.feature_collection.py`) to be
-    executed on the given mvts.
-    :return: a dataframe with the parameters as rows and statistical features as columns.
+    :param df_mvts: a mvts dataframe from which the features are to be extracted.
+    :param features_list : a list of all functions (from `features.feature_collection`) to be
+           executed on the given mvts.
+    :return: a dataframe with the parameters as rows, and statistical features as columns.
     """
     col_names = list(df_mvts)
     df_features = pd.DataFrame(index=col_names, dtype=float)
@@ -64,18 +66,21 @@ def flatten_to_row_df(df: pd.DataFrame) -> pd.DataFrame:
     columns names in the resultant dataframe is derived from the given dataframe df, by combining
     the row and column names of the given dataframe.
 
-    For example, for a given df like the one below:
-    -----------------------------------------------
-        f1    f2    ...
-    p1  val   val   ...
-    p2  val   val   ...
-    ... ...   ...   ...
-    -----------------------------------------------
-    the column names in the output dataframe would be:
-    -----------------------------------------------
-        P1_f1   P1_f2   ... P2_f1   P2_f2   ...
-    1   val     val         val     val
-    -----------------------------------------------
+    For example, for a given df like the one below::
+
+        -----------------------------------------------
+            f1    f2    ...
+        p1  val   val   ...
+        p2  val   val   ...
+        ... ...   ...   ...
+        -----------------------------------------------
+
+    the column names in the output dataframe would be::
+
+        -----------------------------------------------
+            P1_f1   P1_f2   ... P2_f1   P2_f2   ...
+        1   val     val         val     val
+        -----------------------------------------------
 
     :param df: the data frame to be flattened.
     :return: a dataframe with one row and P X F columns, with values similar to the given dataframe.
@@ -92,8 +97,9 @@ def flatten_to_row_df(df: pd.DataFrame) -> pd.DataFrame:
 def split(l: list, n_of_partitions: int) -> list:
     """
     Splits the given list l into n_of_paritions partitions of approximately equal size.
-    :param l:
-    :param n_of_partitions: number of partitions
+
+    :param l: the list to be split.
+    :param n_of_partitions: number of partitions.
     :return: a list of the partitions, where each partition is a list itself.
     """
     k, m = divmod(len(l), n_of_partitions)
