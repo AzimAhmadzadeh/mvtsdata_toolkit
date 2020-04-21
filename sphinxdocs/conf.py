958decbd6c15eedc0bf4b105a4799d234e3a2b98
AzimAhmadzadeh <azim.ahmadzadeh@gmail.com>
2020-04-20 20:20:50 -0400

    Pre-change commit.

--- sphinxdocs/conf.py
+++ sphinxdocs/conf.py
@@ -73,10 +73,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['build/*', 'Thumbs.db', '.DS_Store', '__icon/*', 'dis/*t',
-                    'mvtsdatatoolkit.egg-info/*', 'tests/*', 'CONSTANTS.py',
-                    'bitbucket-pipelines.yml', 'README.md', 'requirements.txt', 'setup.py',
-                    'demo.py', 'LICENCE.txt']
+# exclude_patterns = ['build/*', 'Thumbs.db', '.DS_Store', '__icon/*', 'dis/*t',
+#                     'mvtsdatatoolkit.egg-info/*', 'tests/*', 'CONSTANTS.py',
+#                     'bitbucket-pipelines.yml', 'README.md', 'requirements.txt', 'setup.py',
+#                     'demo.py', 'LICENCE.txt']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
